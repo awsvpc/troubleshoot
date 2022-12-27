c88cf26167fdd35cf662c616a24d077c36359f33
vkonada <70343697+vkonada@users.noreply.github.com>
2022-12-26 22:25:49 -0500

    Update CurlCommands.sh

--- Linux/Network/Curl/CurlCommands.sh
+++ Linux/Network/Curl/CurlCommands.sh
@@ -1,4 +1,11 @@
-TRACE
+## TRACE
 
 CURL -X "TRACE" domain.com
 CURL -X "TRACE" --proxy "127.0.0.1:port" domain.com
+
+## OPTIONS
+
+OPTION method is used to describe the communication option for the target resource
+
+curl -x OPTIONS http://domain.com -i
+
